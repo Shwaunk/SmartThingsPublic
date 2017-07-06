689c9c84f2c2a3098a960947c70e2773c646b399
Shaun Patterson <shwaunk@gmail.com>
2017-07-05 23:16:42 -0500

    template

---
+++ smartapps/shwaunk/sdp-template.src/sdp-template.groovy
@@ -0,0 +1,44 @@
+/**
+ *  SDP Template
+ *
+ *  Copyright 2017 Shaun Patterson
+ *
+ */
+definition(
+    name: "SDP Template",
+    namespace: "Shwaunk",
+    author: "Shaun Patterson",
+    description: "Blank Template",
+    category: "SmartThings Labs",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png") {
+    appSetting "First Setting"
+    appSetting "Second Setting"
+}
+
+
+preferences {
+	section("Title") {
+		// TODO: put inputs here
+	}
+}
+
+def installed() {
+	log.debug "Installed with settings: ${settings}"
+
+	initialize()
+}
+
+def updated() {
+	log.debug "Updated with settings: ${settings}"
+
+	unsubscribe()
+	initialize()
+}
+
+def initialize() {
+	// TODO: subscribe to attributes, devices, locations, etc.
+}
+
+// TODO: implement event handlers
